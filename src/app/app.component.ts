33d6d92bc9e58a0c962be06caaa74f39b5b3325e
natali-gracia <natali1gracia@gmail.com>
2020-11-12 15:59:10 +0200

    added formControl group and created calendar controller

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'ng-form';
+export class AppComponent implements OnInit {  
+  
+  form!: FormGroup 
+  selectedDate: string = ''
+
+  ngOnInit() {
+    this.form = new FormGroup({
+      date: new FormControl('')
+    })
+  }
+
+  getReport() {
+    console.log('Report generated!', this.form)
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {FormsModule} from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -17,6 +17,7 @@ import { DevicesComponent } from './devices/devices.component';
   imports: [
     BrowserModule,
     FormsModule,
+    ReactiveFormsModule,
     BrowserAnimationsModule,
     MaterialModule
   ],
